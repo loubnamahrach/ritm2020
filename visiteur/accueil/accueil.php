28b6ec50b07ea89fe222a33ec253c78e422e71e0
unknown <loubnamahrach1@gmail.com>
2024-02-29 02:28:18 +0100

    ajout de accueil

---
+++ visiteur/accueil/accueil.php
@@ -0,0 +1,523 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Upload image</title>
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/all.min.css">
+  <link rel="stylesheet"  href="../css/style.css">
+
+</head>
+
+<body>
+
+<!-- inclure le header -->
+<?php include '../include/header.php'?>
+
+<section class="section-image" style="background: url('../images/bgtest5.jpg') center/cover no-repeat; height: 115vh; display: flex; align-items: center; justify-content: center; color: #ffffff;">
+</section>
+
+
+
+
+<div class="space overflow-hidden" id="about-sec" style="margin-top: 50px;">
+        <div class="container" >
+            <div class="row align-items-center">
+                <div class="col-xl-6">
+                    <img src="../images/ritm.gif" style="width: 600px;">
+                </div>
+                <div class="col-xl-6">
+                    <div class="title-area mb-30">
+                        <span class="sub-title" style="color: #0d5ef4; font-weight: bold;"><i class="fa fa-book me-2"></i> A propos de notre laboratoire</span>
+                        <h2 class="sec-title">Bienvenue au laboratoire <strong style="color: #0d5ef4;">RITM.</strong> </h2>
+                </div>              
+                <p>
+                Le laboratoire de recherche RITM (Réseaux, Informatique, Télécommunication et Multimédia) est un laboratoire de recherche de l’Ecole Supérieure de Technologie de Casablanca. Créée en 2007, le labo RITM est caractérisé par sa couverture large du domaine des sciences et technologies de l’information et de la communication. L’informatique, les réseaux, les télécommunications et les énergies renouvelables forment le cœur des thématiques du Laboratoire RITM. Pleinement intégré au projet de l’université Hassan II, le laboratoire RITM a également pour ambition de contribuer à certains de ses objectifs majeurs qui sont, le renforcement de la formation par la recherche, l’émergence de recherches interdisciplinaires, le développement du potentiel d’innovation et de transfert technologique ainsi que le rayonnement international.
+                </p>                   
+                </div>
+            </div>
+        </div>
+    </div>
+  
+
+<br>
+<br>
+
+  <section class="space-top feature-sec" style="background-image: url(../images/category_bg_1.jpg) ;background-repeat: repeat;">
+    <br>
+    <h1 class="titre">Les thématiques du laboratoire RITM</h1>
+        <div class="container" >
+            <div class="row gy-4 justify-content-center" >
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                            <img src="../images/1.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">Systèmes Distribués Big Data</h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                            <img src="../images/2.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">Réseaux de télécommunications</h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                            <img src="../images/3.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">cyber-sécurité</h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                             <img src="../images/4.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">efficacité énergétique</h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>  
+                 <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                            <img src="../images/5.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">réseaux de capteurs sans fil et leurs applications</h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                            <img src="../images/6.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">smart city</h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                            <img src="../images/7.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">énergies renouvelables</h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6">
+                    <div class="feature-list">
+                        <div class="feature-list_icon">
+                             <img src="../images/8.png" alt="" >
+                        </div>
+                        <h3 class="feature-list_title">Antennes </h3>
+                        <p class="feature-list_text"></p>
+                    </div>
+                </div>  
+            
+<div class="col-xl-4 col-md-6">
+    <div class="feature-list">
+        <div class="feature-list_icon">
+            <img src="../images/9.png" alt="">
+        </div>
+        <h3 class="feature-list_title">les sciences de données et ingénierie des systèmes</h3>
+        <p class="feature-list_text"></p>
+    </div>
+</div>
+
+
+            </div>
+        </div>
+    </section>
+
+
+<div class="space overflow-hidden" id="about-sec" style="margin-top: 0px; background-color: #f8f9fa; padding: 50px;">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="title-area mb-4">
+                    <h2 class="titre" style="color: #333; font-size: 36px;text-align: left;">Recherche et Partenariats</h2>
+                </div>
+                <div class="content-area">
+                    <p style="color: #666; font-size: 18px; line-height: 1.6;">Les différents axes de recherche mettent en évidence et répondent aux besoins en développement technologique du Maroc, intrinsèquement porteurs de forts potentiels de valorisation.</p>
+                    <p style="color: #666; font-size: 18px; line-height: 1.6;">Ces axes transversaux s'appuient souvent sur des partenariats extérieurs très structurants : pluridisciplinarité scientifique, collaborations industrielles nationales ou internationales, etc. et s'appuient sur les collaborations transversales au sein des départements scientifiques pour bâtir des offres de compétences plus complètes.</p>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="image-container">
+                    <img src="../images/part.png" alt="Partenariats" >
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<section class="space-top feature-sec" style="background-image: url(../images/.jpg) ;background-repeat: repeat;margin-top: 50px;">
+    <br>
+    <h1 class="titre">Collaboration Internationale</h1>
+    <div class="container">
+        <div class="row gy-4 justify-content-center">
+            <div class="col-md-4">
+                <div class="info-box">
+                    <img src="../images/illustration1.png" alt="Illustration 1">
+                    
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="info-box">
+                    <img src="../images/illustration2.png" alt="Illustration 2">
+                   
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="info-box">
+                    <img src="../images/illustration3.png" alt="Illustration 3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="space-top feature-sec" >
+    <br>
+
+    <div class="container" style="margin-bottom: 100px;">
+        <div class="row gy-4 justify-content-center">
+            <div class="col-md-8">
+                <div class="">
+                    <p class="cooperation-text">RITM est également membre du consortium de 6 laboratoires constituant le Centre de Recherche TICDev : TIC au service du développement Régional, créé dans le cadre des Centres Thématiques de Recherche CTR lancé par l’UH2C en 2016.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- inclure le footer -->
+<?php include '../include/footer.php'?>
+
+
+
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/bootstrap.bundle.min.js"></script>
+  <script src="../js/all.min.js"></script>
+</body>
+
+</html>
+
+
+<style>
+    
+    
+    #about-sec {
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+.title-area h2 {
+    font-family: 'Arial', sans-serif;
+    font-weight: bold;
+}
+
+.content-area {
+    margin-top: 30px;
+}
+
+.image-container {
+    text-align: center;
+}
+
+
+.cooperation-text {
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 20px;
+    text-align: center;
+    margin-bottom: 20px;
+    font-style: italic;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+
+    .info-box {
+    background-color: #fff;
+    border-radius: 10px;
+    padding: 20px;
+    text-align: center;
+    margin-bottom: 20px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.info-box img {
+    max-width: 100px;
+    margin-bottom: 10px;
+}
+
+
+
+
+    .titre {text-align: center;
+            font-size: 36px;
+            font-weight: bold;
+             background: radial-gradient(circle, #212529, #0021ff);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            margin-bottom: 60px;
+        }
+    .container img {
+  max-width: 100%; /* Assurez-vous que l'image ne dépasse pas la largeur du conteneur */
+  max-height: 100%; /* Assurez-vous que l'image ne dépasse pas la hauteur du conteneur */
+}
+/* Feature 3 ---------------------------------- */
+.feature-list {
+  padding: 40px 30px 55px 30px;
+  background-color: var(--theme-color);
+  box-shadow: 0px 6px 30px rgba(0, 6, 18, 0.06);
+  text-align: center;
+  position: relative;
+  z-index: 2;
+  margin-bottom: 25px;
+  height: 250px;
+}
+
+.feature-list:after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: var(--white-color);
+  z-index: -1;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+}
+
+.feature-list_icon {
+  width: 80px;
+  height: 80px;
+  line-height: 80px;
+  font-size: 36px;
+  color: var(--white-color);
+  background-color: var(--theme-color);
+  border-radius: 50%;
+  margin: 0 auto 25px auto;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+  position: relative;
+  z-index: 2;
+  box-shadow: 0px 4px 20px rgba(13, 94, 244, 0.5);
+}
+
+.feature-list_title {
+  font-size: 24px;
+  margin-bottom: 15px;
+  margin-top: -0.24em;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+   background: radial-gradient(circle, #212529, #0021ff);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+}
+
+.feature-list_title a {
+  color: var(--title-color);
+  text-decoration: none;
+
+}
+
+.feature-list_text {
+  margin-bottom: -0.45em;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+
+}
+
+.feature-list .icon-btn {
+  --btn-size: 50px;
+  background-color: var(--theme-color);
+  color: var(--white-color);
+  box-shadow: 0px 4px 18px rgba(13, 94, 244, 0.24);
+  position: absolute;
+  bottom: -25px;
+  left: 50%;
+  margin-left: -25px;
+}
+
+.feature-list:hover:after {
+  height: 0;
+}
+
+.feature-list:hover .feature-list_icon {
+  background-color: var(--white-color);
+  color: var(--theme-color);
+}
+
+.feature-list:hover .feature-list_title a {
+  color: var(--white-color);
+}
+
+.feature-list:hover .feature-list_text {
+  color: var(--white-color);
+}
+
+.feature-list:hover .icon-btn {
+  background-color: var(--white-color);
+  color: var(--theme-color);
+  -webkit-animation: jumpIcon 1s linear infinite;
+          animation: jumpIcon 1s linear infinite;
+}
+
+.feature-sec {
+  background-image: -webkit-linear-gradient(right, var(--smoke-color), var(--smoke-color));
+  background-image: linear-gradient(to left, var(--smoke-color), var(--smoke-color));
+  background-size: 100% 60%;
+  background-position: top center;
+  background-repeat: no-repeat;
+}
+
+/* Admission 1 ------------------------------*/
+.admission-card {
+  padding: 30px;
+  background-color: var(--white-color);
+  box-shadow: 0px 4px 50px rgba(0, 6, 18, 0.1);
+  border-radius: 10px;
+  text-align: center;
+}
+
+.admission-card_img {
+  margin-bottom: 23px;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.admission-card_img img {
+  width: 100%;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+}
+
+.admission-card:hover .admission-card_img img {
+  -webkit-transform: scale(1.1);
+      -ms-transform: scale(1.1);
+          transform: scale(1.1);
+}
+
+/* Extra small devices */
+@media (max-width: 375px) {
+  .admission-card {
+    padding: 20px;
+  }
+}
+
+.feature-list {
+  padding: 40px 30px 55px 30px;
+  background-color: var(--theme-color);
+  box-shadow: 0px 6px 30px rgba(0, 6, 18, 0.06);
+  text-align: center;
+  position: relative;
+  z-index: 2;
+  margin-bottom: 25px;
+}
+
+.feature-list:after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: var(--white-color);
+  z-index: -1;
+  -webkit-transition: 0.8s ease-in-out;
+  transition: 0.8s ease-in-out;
+}
+
+.feature-list_icon {
+  width: 80px;
+  height: 80px;
+  line-height: 80px;
+  font-size: 36px;
+  color: var(--white-color);
+  background-color: var(--theme-color);
+  border-radius: 50%;
+  margin: 0 auto 25px auto;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+  position: relative;
+  z-index: 2;
+  box-shadow: 0px 4px 20px rgba(13, 94, 244, 0.5);
+}
+
+.feature-list_title {
+  font-size: 24px;
+  margin-bottom: 15px;
+  margin-top: -0.24em;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+}
+
+.feature-list_title a {
+  color: var(--title-color);
+}
+
+.feature-list_text {
+  margin-bottom: -0.45em;
+  -webkit-transition: 0.4s ease-in-out;
+  transition: 0.4s ease-in-out;
+}
+
+.feature-list .icon-btn {
+  --btn-size: 50px;
+  background-color: var(--theme-color);
+  color: var(--white-color);
+  box-shadow: 0px 4px 18px rgba(13, 94, 244, 0.24);
+  position: absolute;
+  bottom: -25px;
+  left: 50%;
+  margin-left: -25px;
+}
+
+.feature-list:hover:after {
+    background-color: #0d5ef485;
+}
+
+.feature-list:hover .feature-list_icon {
+  background-color: var(--white-color);
+  color: var(--theme-color);
+}
+
+.feature-list:hover .feature-list_title a {
+  color: var(--white-color);
+}
+
+.feature-list:hover .feature-list_text {
+  color: var(--white-color);
+}
+
+.feature-list:hover .icon-btn {
+  background-color: var(--white-color);
+  color: var(--theme-color);
+  -webkit-animation: jumpIcon 1s linear infinite;
+          animation: jumpIcon 1s linear infinite;
+}
+
+.feature-sec {
+  background-image: -webkit-linear-gradient(right, var(--smoke-color), var(--smoke-color));
+  background-image: linear-gradient(to left, var(--smoke-color), var(--smoke-color));
+  background-size: 100% 60%;
+  background-position: top center;
+  background-repeat: no-repeat;
+}
+</style>
